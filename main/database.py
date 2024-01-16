7967c4c36b5c396394923ecf38a9b55945e6fcf7
ilya90906767 <betyaev.ilya2004@gmail.com>
2024-01-16 17:11:01 +0300

    Upload function

--- main/database.py
+++ main/database.py
@@ -24,13 +24,43 @@ def create_table():
             )
             """
         curs.execute(query)
-        print("Table created successfully.")
+        
+        query = """
+        CREATE TABLE IF NOT EXISTS books(
+                book_id SERIAL PRIMARY KEY,
+                user_id SERIAL,
+                book_name VARCHAR(255) DEFAULT 'Unknown',
+                book_author VARCHAR(255) DEFAULT 'Unknown',
+                book_picture VARCHAR(255) DEFAULT 'NoPicture',
+                book_src VARCHAR(255),
+                current_progress INT DEFAULT 0,
+                data_added TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (user_id) REFERENCES users(user_id) 
+                )
+        """
+        
+        curs.execute(query)
+        print("Tables created successfully.")
 
     except Exception as error:
         print(f"An error occurred: {error}")
+        
+
+def migrate():
+    curs = connect()
+    curs.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
+    tables = curs.fetchall()
+    print(tables, 'will be deleted')
+    
+    for table in tables:
+        curs.execute(f"DROP TABLE IF EXISTS {table[0]} CASCADE")
+    
+    
+    create_table()
+    
 
 
-def create_user(user_id):
+def add_user(user_id):
     curs = connect()
     if curs is not None:
         try:
@@ -63,13 +93,52 @@ def is_user_exist(user_id):
 
 def get_users():
     curs = connect()
+    curs.execute("SELECT * FROM users")
+    users = curs.fetchall()
+    for user in users:
+        print(user)
+        
+def get_books():
+    curs = connect()
+    curs.execute("SELECT * FROM books")
+    books = curs.fetchall()
+    for book in books:
+        print(book)
+        
+def get_tables():
+    curs = connect()
     curs.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='public'")
     tables = curs.fetchall()
 
 
     for table in tables:
         print(table)
-    """curs.execute("SELECT * FROM users")
-    users = curs.fetchall()
-    for user in users:
-        print(user)"""
+        
+def delete_user(user_id):
+    curs = connect()
+    
+    curs.execute(f"DELETE FROM users WHERE user_id = {user_id}")
+    print(f"User with user_id = {user_id} was deleted")
+    
+    
+
+
+def add_book(user_id,book_name,book_src):
+    curs = connect()
+    if curs is not None:
+        try:
+                curs.execute(f"INSERT INTO books(book_name, book_src, user_id) VALUES ('{book_name}','{book_src}',{user_id})")
+                print('BOOK WAS ADDED', book_name)
+        except Exception as error:
+            print(f"An error occurred when trying to execute the query: {error}")
+            return None
+    else:
+        return None
+    
+    
+
+#create_table()
+#add_book(111,'TestBook','URLTest')
+#delete_user(111111)
+get_users()
+get_books()
--- telegram.py
+++ telegram.py
@@ -5,7 +5,6 @@ from telebot import types
 from main.book import Book
 from main.database import *
 
-create_table()
 Martin_Eden = Book('main/books/Martin_Eden.txt')
 
 load_dotenv()
@@ -21,12 +20,34 @@ def send_welcome(message):
         bot.send_message(message.chat.id, "What are you goin to read today?")
     if is_user_exist(message.chat.id) == False:
         try: 
-            create_user(message.chat.id)
+            add_user(message.chat.id)
             bot.send_message(message.chat.id, "New at this platform? Need some help? Type /help :)")
         except Exception as error:
             bot.send_message(message.chat.id, "oops... some error ocured...")
             
 
+
+@bot.message_handler(commands=['upload'])
+def upload_welcome(message):
+    bot.send_message(message.chat.id, "Okay, just upload your book. Now we can process only .txt files")
+    bot.register_next_step_handler(message, process_file)
+    
+def process_file(message):
+    try: 
+        chat_id = message.chat.id
+        file_info = bot.get_file(message.document.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+        src = f"/home/ilyabetyaev/petprojects/BookEng/main/books/" + message.document.file_name
+        
+        with open(src, "wb") as new_file:
+            new_file.write(downloaded_file)
+        
+        add_book(message.chat.id,message.document.file_name,src)
+        bot.reply_to(message,"File was upload succesfully")
+    except Exception as e:
+        bot.reply_to(message,e)
+        
+    
 @bot.message_handler(commands=['help'])
 def send_welcome(message): 
     bot.reply_to(message, "There will be help message")
